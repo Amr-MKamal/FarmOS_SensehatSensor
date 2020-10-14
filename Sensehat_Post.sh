b1f67bc9185c1a851d0c135d9d1b4beca7ee2426
Amr-MKamal <thedamger_37@hotmail.com>
2020-10-14 13:58:50 +0200

    Update Sensehat_Post.sh

--- Sensehat_Post.sh
+++ Sensehat_Post.sh
@@ -3,6 +3,9 @@ sense_csv --timestamp-format %m%d%H%M%S  test_1.hat - | cut -d, -f1-4,15-17 > ex
 INPUT=experiment.csv
 OLDIFS=$IFS
 pitod=57.2957796
+Public_key=
+Private_key=
+Url=
 IFS=','
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 while read timestamp pressure pressure_temp humidity orient_x orient_y orient_z
@@ -15,6 +18,6 @@ do
 	pitch=$(echo  "$orient_y * $pitod" | bc | awk '{printf "%f",$0}')
 	orient_z=$(echo "$orient_z" | awk '{printf "%d",$0}')
 	yaw=$(echo  "$orient_z * $pitod" | bc | awk '{printf "%f",$0}')
-	echo '{"Temprature":'$pressure_temp',"Humidity " : '$humidity',"Pressure " : '$pressure',"Roll ":'$roll',"Pitch ":'$pitch',"Yaw ":'$yaw',"TimeStamp":'$timestamp'}' | farmos-cli-0.2.0/push --url https://farmtopia.farmos.net --feed sense-hat --public_key c29ab2e9f58d7cab13fa1d072766ee42 --private_key a39fd3bafc1a3dea78ad965e77cd8082 
+	echo '{"Temprature":'$pressure_temp',"Humidity " : '$humidity',"Pressure " : '$pressure',"Roll ":'$roll',"Pitch ":'$pitch',"Yaw ":'$yaw',"TimeStamp":'$timestamp'}' | farmos-cli-0.2.0/push --url $Url --feed sense-hat --public_key $Public_key --private_key Private_key
 done < $INPUT
 IFS=$OLDIFS
